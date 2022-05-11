e007e1b1001d1a7bb7c37af094ca9494e5ffc6c2
Mahmoud khalifa <eng.mahmoudkhalifah@gmail.com>
2022-05-11 12:06:36 +0200

    edit account added

---
+++ FB/Controllers/ExistingChecker.cs
@@ -0,0 +1,43 @@
+﻿using FB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FB.Controllers
+{
+    public class ExistingChecker
+    {
+        
+        [NonAction]
+        public static bool isEmailExist(string email)
+        {
+            DataContext db = new DataContext();
+            return db.Users.Where(u => u.email == email).ToList().Count() != 0;
+        }
+
+        [NonAction]
+        public static bool isEmailExistEdit(string email)
+        {
+            DataContext db = new DataContext();
+            int id = int.Parse(HttpContext.Current.User.Identity.Name);
+            return db.Users.Where(u => u.email == email && u.id != id).ToList().Count() != 0;
+        }
+
+        [NonAction]
+        public static bool isPhoneNumberExist(string phoneNumber)
+        {
+            DataContext db = new DataContext();
+            return db.Users.Where(u => u.phone_number == phoneNumber).ToList().Count() != 0;
+        }
+
+        [NonAction]
+        public static bool isPhoneNumberExistEdit(string phoneNumber)
+        {
+            DataContext db = new DataContext();
+            int id = int.Parse(HttpContext.Current.User.Identity.Name);
+            return db.Users.Where(u => u.phone_number == phoneNumber && u.id != id).ToList().Count() != 0;
+        }
+    }
+}
--- FB/Controllers/UserController.cs
+++ FB/Controllers/UserController.cs
@@ -1,6 +1,8 @@
 ﻿using FB.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -37,6 +39,67 @@ namespace FB.Controllers
             }
             return View();
             
+        }  
+
+        [Authorize]
+        [HttpGet]
+        public ActionResult EditProfile()
+        {
+            int userId = int.Parse(User.Identity.Name);
+            User user = db.Users.Where(u => u.id == userId).ToList()[0];
+            user.confirmPassword = user.password;
+            return View(user);
+        }
+        
+        [HttpPost]
+        public ActionResult EditProfile(User user, HttpPostedFileBase image)
+        {
+            //if validation is ok
+            if (ModelState.IsValid)
+            {
+                //if email already exists in our database
+                if (ExistingChecker.isEmailExistEdit(user.email))
+                {
+                    ModelState.AddModelError("EmailExist", "Email already taken!");
+                    return View(user);
+                }
+                //if phone number already exists in our database
+                if (ExistingChecker.isPhoneNumberExistEdit(user.phone_number))
+                {
+                    ModelState.AddModelError("PhoneNumberExist", "Phone number already taken!");
+                    return View(user);
+                }
+                if (image != null && image.ContentLength != 0)
+                {
+                    string ext = Path.GetExtension(image.FileName);
+                    var exts = new[] { "jpg", "jpeg", "png" };
+                    if (exts.Contains(ext.Substring(1)))
+                    {
+                        string imageGuid = Guid.NewGuid().ToString();
+                        user.profile_picture = imageGuid + ext;
+                        image.SaveAs(Server.MapPath($"~/uploads/profile_pictures/{user.profile_picture}"));
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("NotValidImage", "File extention is not valid! try uplaod png,jpg or jpeg image");
+                        return View(user);
+                    }
+
+                }
+                /*u.id = user.id;
+                u.first_name = user.first_name;
+                u.last_name = user.last_name;
+                u.email = user.email;
+                u.phone_number = user.phone_number;
+                u.password = user.password;
+                u.confirmPassword = user.confirmPassword;*/
+
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(user);
+
         }
     }
 }
