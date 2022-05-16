502c0eacc8f8fc579484d2e3b02de0ac4eba07c7
Mahmoud khalifa <eng.mahmoudkhalifah@gmail.com>
2022-05-16 17:07:14 +0200

    navbar check auth

---
+++ FB/ViewModels/UserLoginUserViewModel.cs
@@ -0,0 +1,14 @@
+﻿using FB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FB.ViewModels
+{
+    public class UserLoginUserViewModel
+    {
+        public UserLogin userLogin { get; set; }
+        public User user { get; set; }
+    }
+}
